24a1c733377d2203cd9c91028552b503e375687a
Kveini <Kveini77@gmail.com>
2024-03-22 16:44:12 +0600

    HW1

--- handlers/questionnarie.py
+++ handlers/questionnarie.py
@@ -1,14 +1,66 @@
 from aiogram import types, Dispatcher
-from aiogram.dispatcher import FSMContext
-
 from config import bot
+from keyboards.questionnarie import questionnaire_keyboard, questionnaire_keyboard2, questionnaire_keyboard3
 
 
 async def questionnaire_start_call(call: types.CallbackQuery):
     await call.message.delete()
     await bot.send_message(
         chat_id=call.from_user.id,
-        text="Python or Mojo?"
+        text="do you like light novels?",
+        reply_markup=await questionnaire_keyboard()
+    )
+
+
+async def yes_call(call: types.CallbackQuery):
+    await call.message.delete()
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text="wow me too\n12 or 24",
+        reply_markup=await questionnaire_keyboard2()
+    )
+
+
+async def no_call(call: types.CallbackQuery):
+    await call.message.delete()
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text="pon\n12 or 24?",
+        reply_markup=await questionnaire_keyboard2()
+    )
+
+
+async def num12_call(call: types.CallbackQuery):
+    await call.message.delete()
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text="pon\ndo you like initial d?",
+        reply_markup=await questionnaire_keyboard3()
+    )
+
+
+async def num24_call(call: types.CallbackQuery):
+    await call.message.delete()
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text="pon\ndo you like initial d?",
+        reply_markup=await questionnaire_keyboard3()
+    )
+
+
+async def love_call(call: types.CallbackQuery):
+    await call.message.delete()
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text="thank you for completing the survey"
+    )
+
+
+async def dont_love_call(call: types.CallbackQuery):
+    await call.message.delete()
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text="thank you for completing the survey"
     )
 
 
@@ -17,3 +69,28 @@ def register_questionnaire_handlers(dp: Dispatcher):
         questionnaire_start_call,
         lambda call: call.data == "start_questionnarire"
     )
+    dp.register_callback_query_handler(
+        yes_call,
+        lambda call: call.data == "yes"
+    )
+    dp.register_callback_query_handler(
+        no_call,
+        lambda call: call.data == "no"
+    )
+    dp.register_callback_query_handler(
+        num12_call,
+        lambda call: call.data == "num12"
+    )
+    dp.register_callback_query_handler(
+        num24_call,
+        lambda call: call.data == "num24"
+    )
+    dp.register_callback_query_handler(
+        love_call,
+        lambda call: call.data == "love"
+    )
+    dp.register_callback_query_handler(
+        dont_love_call,
+        lambda call: call.data == "dont_love"
+    )
+
--- keyboards/questionnarie.py
+++ keyboards/questionnarie.py
@@ -4,12 +4,49 @@ from aiogram.types import (
 )
 
 
-async def start_menu_keyboard():
+async def questionnaire_keyboard():
     markup = InlineKeyboardMarkup()
 
-    questionnarie_button = InlineKeyboardButton(
-        "Python",
-        callback_data="python"
+    yes_button = InlineKeyboardButton(
+        "Yes",
+        callback_data="yes"
     )
-    markup.add(questionnarie_button)
+    no_button = InlineKeyboardButton(
+        "No",
+        callback_data="no"
+    )
+    markup.add(yes_button)
+    markup.add(no_button)
+    return markup
+
+
+async def questionnaire_keyboard2():
+    markup = InlineKeyboardMarkup()
+
+    num12_button = InlineKeyboardButton(
+        "12",
+        callback_data="num12"
+    )
+    num24_button = InlineKeyboardButton(
+        "24",
+        callback_data="num24"
+    )
+    markup.add(num12_button)
+    markup.add(num24_button)
+    return markup
+
+
+async def questionnaire_keyboard3():
+    markup = InlineKeyboardMarkup()
+
+    love_button = InlineKeyboardButton(
+        "Love",
+        callback_data="love"
+    )
+    dont_love_button = InlineKeyboardButton(
+        "Dont love",
+        callback_data="dont_love"
+    )
+    markup.add(love_button)
+    markup.add(dont_love_button)
     return markup
